c28d445a66fb8abebfa30a62771cafac61d84f93
abhishek-7262 <abhishek.7262020269@gmail.com>
2025-10-26 17:34:56 +0530

    update in users schema

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -13,7 +13,10 @@ export class User {
     email: string;
 
     @Prop({ required: true })
-    password: string
+    password: string;
+
+    @Prop({ enum: ['user', 'admin'], default: 'user' })
+    role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
