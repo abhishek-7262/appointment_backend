0a6428edbc31feddf509532b6624f4778eb7447e
abhishek-7262 <abhishek.7262020269@gmail.com>
2025-11-08 12:50:47 +0530

    redis setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,24 +9,42 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SlotsModule } from './slots/slots.module';
 import { BookingsModule } from './bookings/bookings.module';
 
+import { redisStore } from 'cache-manager-redis-yet';
+
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+          ttl: 60 * 5, // default TTL = 5 minutes
+        }),
+      }),
+    }),
+
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
       useFactory: async () => ({
         uri: process.env.MONGODB_URI,
         connectionFactory: (connection) => {
           connection.on('connected', () => {
-            console.log('connected to db')
+            console.log('connected to db');
           });
           return connection;
-        }
-      })
+        },
+      }),
     }),
 
-
-    UsersModule, AuthModule, SlotsModule, BookingsModule],
+    UsersModule,
+    AuthModule,
+    SlotsModule,
+    BookingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
